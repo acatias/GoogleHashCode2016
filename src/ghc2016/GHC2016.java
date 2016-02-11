b830a3336d1b0eb359edcd34b58feef85c548110
Aurelian Lavric <aurelian.lavric@sap.com>
2016-02-11 21:55:07 +0100

    add dists comp

--- src/ghc2016/GHC2016.java
+++ src/ghc2016/GHC2016.java
@@ -13,13 +13,19 @@ public class GHC2016 {
 		
 		id1.readFromFile("busy_day.in");
 		
+		Dists dists1 = new Dists(id1);
+		
 		InputData id2 = new InputData();
 		
 		id2.readFromFile("redundancy.in");
 		
+		Dists dists2 = new Dists(id2);
+		
 		InputData id3 = new InputData();
 		
 		id3.readFromFile("mother_of_all_warehouses.in");
+		
+		Dists dists3 = new Dists(id3);
 		
 		long endTime = System.nanoTime();
 		
