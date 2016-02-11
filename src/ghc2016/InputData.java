0ecf7aaf37dbc42685e690113f3a93b8bde7fc4f
Aurelian Lavric <aurelian.lavric@sap.com>
2016-02-11 20:30:32 +0100

    t2

--- src/ghc2016/InputData.java
+++ src/ghc2016/InputData.java
@@ -7,16 +7,19 @@ public class InputData {
 	public int nDrones;
 	public int nTurns;
 	public int maxLoad;
+	
 	public int nProdTypes;
 	public int[] aProdWeights;
+	
 	public int nWHs;
 	public int[] aWHRows;
 	public int[] aWHCols;
-	public int[][] mWHProdTypes;
+	public int[][] mWHProdTypes; // nWHs x nProdTypes
+	
 	public int nOrders;
 	public int[] aOrderRows;
 	public int[] aOrderCols;
 	public int[] nOrderItems; // not for persistance
-	public int[][] aOrderProdTypes;
+	public int[][] aOrderProdTypes; // nOrders x nProdTypes
 
 }
